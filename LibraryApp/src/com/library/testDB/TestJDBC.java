265f650c2103b582792ad6d875fbf02e875fb9a0
danilo <danilo@mymail.com>
2017-08-06 15:06:01 +0200

    Change-Id: I969ff786626bc0b9d258a4224e6a0046fbf8f7bb

---
+++ LibraryApp/src/com/library/testDB/TestJDBC.java
@@ -0,0 +1,29 @@
+package com.library.testDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBC {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/db_library?useSSL=false";
+		String user = "lbuser";
+		String pass = "lbuser";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn =
+					DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successful!!!");
+			
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
+	}
+
+}
