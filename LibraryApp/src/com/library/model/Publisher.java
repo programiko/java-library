6f6e8e39e5a8e5262d1f8e8b460a2c409ffa966c
Marko Lucic <lucicmarko93@gmail.com>
2017-08-17 12:24:41 +0200

    Publisher functionality, publisher.jsp, edit style.css.

---
+++ LibraryApp/src/com/library/model/Publisher.java
@@ -0,0 +1,68 @@
+package com.library.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="publishers")
+public class Publisher {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "phone")
+	private String phone;
+	
+	public Publisher() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "Publishers [id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + "]";
+	}
+	
+	
+}
---
+++ LibraryApp/src/com/library/service/PublisherServiceImpl.java
@@ -0,0 +1,46 @@
+package com.library.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.library.dao.PublisherDAO;
+import com.library.model.Publisher;
+
+@Service
+public class PublisherServiceImpl implements PublisherService {
+	
+	@Autowired
+	private PublisherDAO publisherDAO;
+
+	@Override
+	@Transactional
+	public void addPublisher(Publisher p) {
+		publisherDAO.addPublisher(p);
+
+	}
+
+	@Override
+	@Transactional
+	public List<Publisher> getPublisher() {
+		
+		return publisherDAO.getPublisher();
+	}
+
+	@Override
+	@Transactional
+	public Publisher getPublisherById(int id) {
+		// TODO Auto-generated method stub
+		return publisherDAO.getPublisherById(id);
+	}
+
+	@Override
+	@Transactional
+	public void removePublisher(int id) {
+		publisherDAO.removePublisher(id);
+
+	}
+
+}
