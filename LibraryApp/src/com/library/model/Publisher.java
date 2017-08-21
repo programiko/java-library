21f07dc27b35a29873c3cd06e46790bdb6d466a8
Marko Lucic <lucicmarko93@gmail.com>
2017-08-21 12:05:15 +0200

    Add connection on Publisher to Book

--- LibraryApp/src/com/library/model/Publisher.java
+++ LibraryApp/src/com/library/model/Publisher.java
@@ -1,10 +1,16 @@
 package com.library.model;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -64,5 +70,25 @@ public class Publisher {
 		return "Publishers [id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + "]";
 	}
 	
+@OneToMany(mappedBy="",cascade=
+		{CascadeType.PERSIST,CascadeType.MERGE,
+				CascadeType.DETACH,CascadeType.REFRESH})
 	
+	private List<Book> books;
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+	
+	public void add(Book tempBook) {
+		if(books == null) {
+			books = new ArrayList<>();	
+		}
+		books.add(tempBook);
+		tempBook.setPublisher(this);
+	} 
 }
