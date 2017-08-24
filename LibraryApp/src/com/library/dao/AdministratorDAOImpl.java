14d300582fe07ccf20659ab517225b0895cb956d
danilo <danilo@mymail.com>
2017-08-24 22:32:00 +0200

    fix errors...

--- LibraryApp/src/com/library/dao/AdministratorDAOImpl.java
+++ LibraryApp/src/com/library/dao/AdministratorDAOImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -15,7 +16,7 @@ public class AdministratorDAOImpl implements AdministratorDAO {
 	@Autowired
     private SessionFactory sessionFactory;
     
-    @Override
+
 	
 	@SuppressWarnings("unchecked")
 	@Override
